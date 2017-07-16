34cade0714a81eee565c34d31d597faf619f32e6
jason <jason.gumarjadi@gmail.com>
2017-07-16 14:15:34 +0700

    JAVA Added RecyclerViewDataAdapter Added SectionDataModel Added SectionListDataAdapter Added SingleItemModel Added StudentSignIn Added StudentSignIn
    XML
    Added list_item
    Added list_single_card
    Added student_sign_in
    Added teacher_sign_in
    
    Updated activity_marketplace_main (XML)
    Updated MarketplaceMain (Java)

--- app/src/main/java/com/example/william/stapp/com/stapp/market/SectionDataModel.java
+++ app/src/main/java/com/example/william/stapp/com/stapp/market/SectionDataModel.java
@@ -28,4 +28,3 @@ public class SectionDataModel {
         this.allItemsInSection = allItemsInSection;
     }
 }
-
--- app/src/main/java/com/example/william/stapp/com/stapp/market/SectionListDataAdapter.java
+++ app/src/main/java/com/example/william/stapp/com/stapp/market/SectionListDataAdapter.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.william.stapp.R;
+import com.example.william.stapp.com.stapp.market.SingleItemModel;
 
 import java.util.ArrayList;
 
@@ -20,22 +21,22 @@ import java.util.ArrayList;
 public class SectionListDataAdapter extends RecyclerView.Adapter<SectionListDataAdapter.SingleItemRowHolder> {
 
     private ArrayList<SingleItemModel> itemList;
-    private mContext context;
+    private Context mContext;
 
-    public SectionListDataAdapter(Context context, ArrayList<SingleItemModel> itemList){
+    public SectionListDataAdapter(Context context, ArrayList<SingleItemModel> itemList) {
         this.itemList = itemList;
         this.mContext = context;
     }
 
     @Override
-    public SingleItemRowHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+    public SingleItemRowHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_single_card, null);
         SingleItemRowHolder mh = new SingleItemRowHolder(v);
         return mh;
     }
 
     @Override
-    public void onBindViewHolder(SingleItemRowHolder holder, int i){
+    public void onBindViewHolder(SingleItemRowHolder holder, int i) {
 
         SingleItemModel singleItem = itemList.get(i);
         holder.tvTitle.setText(singleItem.getName());
@@ -47,23 +48,28 @@ public class SectionListDataAdapter extends RecyclerView.Adapter<SectionListData
                 .error(R.drawable.bg)
                 .into(feedListRowHolder.thumbView);*/
     }
+
     @Override
-    public int getItemCount(){
+    public int getItemCount() {
         return (null != itemList ? itemList.size() : 0);
     }
 
-    public class SingleItemRowHolder(View view){
-        super(view);
+    public class SingleItemRowHolder extends RecyclerView.ViewHolder {
+        protected TextView tvTitle;
+        protected ImageView itemImage;
 
-        this.tvTitle = (TextView)view.findViewById(R.id.tvTitle);
-        this.itemImage = (ImageView)view.findViewById(R.id.itemImage);
+        public SingleItemModel(View view) {
+            super(view);
+            this.tvTitle = (TextView) view.findViewById(R.id.tvTitle);
+            this.itemImage = (ImageView) view.findViewById(R.id.itemImage);
 
-        view.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-                Toast.makeText(v.getContext(),tvTitle.getText(), Toast.LENGTH_SHORT).show();
-            }
-        });
+                    Toast.makeText(v.getContext(), tvTitle.getText(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
